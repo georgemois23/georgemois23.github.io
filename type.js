f999d36fd50d29090fa75357707e03ed7a69e31d
GEORGIOS MOYSIADIS <georgemois23@gmail.com>
2024-05-01 00:58:20 +0300

    changes to js

--- type.js
+++ type.js
@@ -1,22 +1,18 @@
-var typed1 = new Typed(".auto-type1", {
-    strings: ["Who is George????"],
-    typeSpeed: 150,
-    loop: false,
-    onComplete: function() {
+
         var typed2 = new Typed(".auto-type2", {
             strings: ["Hello!"],
-            typeSpeed: 150,
+            typeSpeed: 50,
             loop: false,
             onComplete: function() 
             {
                 var typed3 = new Typed(".auto-type3", {
                     strings: ["My name is George Moysiadis, I come from Greece and I am a"],
-                    typeSpeed: 150,
+                    typeSpeed: 50,
                     loop: false,
                     onComplete: function() {
                         var typed4 = new Typed(".auto-type4", {
                             strings: ["web developer.", "student of Computer Science."],
-                            typeSpeed: 200,
+                            typeSpeed: 100,
                             backSpeed: 60,
                             loop: false
                         });
@@ -24,5 +20,4 @@ var typed1 = new Typed(".auto-type1", {
                 });
             }
         });
-    }
-});
+    
